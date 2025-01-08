5e5ba9f5ce403c07bd6ab82d1484cdd2aefd33b1
ChristianRodrigo98 <christian.rodrigoalvarez@cieep.com>
2025-01-08 14:24:02 +0100

    Poner el service de cargar los juguetes

--- src/app/components/juguete-list/juguete-list.component.ts
+++ src/app/components/juguete-list/juguete-list.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Juguete } from '../../common/juguete';
+import { JugueteServiceService } from '../../services/juguete-service.service';
 
 @Component({
   selector: 'app-juguete-list',
   standalone: true,
   imports: [],
   templateUrl: './juguete-list.component.html',
-  styleUrl: './juguete-list.component.css'
+  styleUrl: './juguete-list.component.css',
 })
 export class JugueteListComponent {
+  private readonly jugueteService: JugueteServiceService = inject(
+    JugueteServiceService
+  );
 
+  juguetes: Juguete[] = [];
+
+  constructor() {
+    this.loadJuguetes();
+  }
+
+  loadJuguetes() {}
 }
